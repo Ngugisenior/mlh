612ba021bfd81aa727fbdf2ee3a303ee198c6e81
root <root@DESKTOP-I799II6.localdomain>
2021-03-29 01:50:46 +0200

    updates

--- day 1/main.js
+++ day 1/main.js
@@ -1,9 +1,81 @@
 const root = document.querySelector('.root');
 
 const date = new Date().getFullYear();
-const n = createElem('li');
+let n = createElem('li');
 n.innerHTML=`${date}`
 root.appendChild(n);
 function createElem(tag){
     return document.createElement(tag);
 }
+
+
+const images = [
+    {
+        name: "#1",
+        subtitle:"I gif Up!",
+        url: "assets/D__700-1.jpg",
+        description: "People vs Foxes"
+    },
+    {
+        name: "#2",
+        subtitle:"",
+        url: "assets/P__880.jpg",
+        description: "People vs Foxes"
+    },
+    {
+        name: "#3",
+        subtitle:"",
+        url: "assets/P__881.jpg",
+        description: "People vs Foxes"
+    },
+    {
+        name: "#4",
+        subtitle:"",
+        url: "assets/P__882.jpg",
+        description: "People vs Foxes"
+    },
+    {
+        name: "#5",
+        subtitle:"",
+        url: "assets/P__883.jpg",
+        description: "People vs Foxes"
+    },
+    {
+        name: "#6",
+        subtitle:"",
+        url: "assets/P__884.jpg",
+        description: "People vs Foxes"
+    },
+    {
+        name: "#7",
+        subtitle:"",
+        url: "assets/P__885.jpg",
+        description: "People vs Foxes"
+    }
+];
+
+const articles_styles = `width:500px; height:500px;`
+
+const img_styles = `max-height:300px; max-width:300px`;
+
+function getImage(){
+    let n_div = createElem('div');
+    for (i in images){
+        let nss = createElem('article');
+        let ns = createElem('img');
+        let nt_title = createElem('div');
+        nt_title.innerHTML = `<p><strong>${ images[i].name}</strong></p><br/>  <h2><strong>${ images[i].subtitle}</strong></h2> `
+        ns.setAttribute('src', images[i].url);
+        ns.setAttribute('alt', images[i].description);
+        ns.setAttribute('loading', 'lazy');
+        ns.setAttribute('style', img_styles);
+        nss.setAttribute('style', articles_styles);
+        nss.appendChild(nt_title);
+        nss.appendChild(ns);
+        n_div.appendChild(nss);
+    }
+
+    root.appendChild(n_div);
+
+}
+getImage();
